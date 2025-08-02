cd5cd664d6ca8fe21e01c159c717b9eee274fcc1
Kevin Caldwell <kevinc2004@gmail.com>
2025-08-01 20:04:46 -0400

    WIP fsim script

--- fsim.py
+++ fsim.py
@@ -1,18 +1,23 @@
 #!/bin/python3
 
 import argparse
+import sys
+import os
 import subprocess
 
-BUILD_FOLDER = "./build/"
+BUILD_FOLDER = "build"
 
-FSIM_NAME = "cuda_fluidsim"
-PPM_HANDLER_NAME = "ppm_handler"
-TEST_EXE_NAME = "fsim_test"
+FSIM = "cuda_fluidsim"
+PPM_HANDLER= "ppm_handler"
+TEST_EXE = "fsim_test"
 
-FSIM_PATH = BUILD_FOLDER + FSIM_NAME
-PPM_HANDLER_PATH = BUILD_FOLDER + PPM_HANDLER_NAME
-TEST_EXE_PATH = BUILD_FOLDER + TEST_EXE_NAME
-
+def main():
+    os.chdir(BUILD_FOLDER)
+    subprocess.run(["cmake", '..'])
+    subprocess.run(['make', '-j'])
+    os.chdir("../")
+    subprocess.run([f"./{BUILD_FOLDER}/{TEST_EXE}"])
+    subprocess.run([f"./{BUILD_FOLDER}/{FSIM}"])
 
 if __name__ == "__main__":
-    subprocess.run([FSIM_PATH])
+    main()
